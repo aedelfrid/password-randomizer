1bf1baf40a14e08a29d12ce720b1f5b1b9cefcea
aedelfrid <almck51@gmail.com>
2023-05-14 14:19:26 -0300

    successfully debugged issue where generator was only selecting uppercase characters

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = checkCriteria();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -15,3 +15,102 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // ask user, length, whether to use special characters, numbers, letters, upperCase, lowerCase
+
+var password = null;
+var passLen = undefined;
+var criteria = ["lowercase", "uppercase", "numeric", "special"];
+
+const lowercaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const uppercaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const specialCharacters = ["!","@","#","$","%","&","*","(",")","-","_","=","+","'",".","/","<"]
+
+function checkCriteria() {
+  alert("Welcome to the password generator wizard!");
+
+  passLen = prompt("How long would you like the password to be?\nBe aware it must be at least 8 and at most 128 characters.")
+
+  if (!confirm("Would you like the password to include special characters?")) {
+      criteria.pop()
+  };
+  if (!confirm("Would you like the password to include numeric characters?")) {
+      criteria.pop()
+  };
+  if (!confirm("Would you like the password to include uppercase characters?")) {
+      criteria.pop()
+  };
+  if (!confirm("Would you like the password to include lowercase characters?")) {
+      criteria.pop()
+  };
+
+  validate();
+}
+
+function validate() {
+
+  if (!passLen > 8 && !passLen < 128) {
+        alert("You must select a password length of at least 8 characters\nOr at most 128 characters.")
+  }
+
+  if (criteria.length >= 1) {
+    generate()
+  } else {
+    alert("You must select at least one password criteria!\nPlease try again.")
+  }
+}
+
+function generate() {
+  let determiner = null;
+  determiner = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+      if (determiner == 0) {
+          var lower = Math.floor(Math.random() * (26 - 0 + 1)) + 0
+          password = lowercaseCharacters[lower];
+      } else if (determiner == 1) {
+          var upper = Math.floor(Math.random() * (26 - 0 + 1)) + 0;
+          password = uppercaseCharacters[upper];
+      } else if (determiner == 2) {
+          password = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+      } else if (determiner == 3) {
+          var special = Math.floor(Math.random() * (19 - 0 + 1)) + 0;
+          password = specialCharacters[special];
+      }
+    
+  for (let i = 0; i < passLen; i++) {
+      determiner = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+      if (determiner == 0) {
+          var lower = Math.floor(Math.random() * (26 - 0 + 1)) + 0;
+          password += lowercaseCharacters[lower];
+          continue
+      } else if (determiner == 1) {
+          var upper = Math.floor(Math.random() * (26 - 0 + 1)) + 0;
+          password += uppercaseCharacters[upper];
+          continue
+      } else if (determiner == 2) {
+          password += Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+          continue
+      } else if (determiner == 3) {
+          var special = Math.floor(Math.random() * (19 - 0 + 1)) + 0;
+          password += specialCharacters[special];
+          continue
+      }
+    }
+
+    display();
+}
+
+
+function display() {
+    if(password.length = passLen) {
+        alert(password)
+        //later display password on page itself
+    }
+}
+
+
+
+
+// insert other code here 
+// criteria = ["lowercase", "uppercase", "numeric", "special"]; should always be at the bottom so that it refreshes the variable
+
+
+criteria = ["lowercase", "uppercase", "numeric", "special"];
+password = null;
