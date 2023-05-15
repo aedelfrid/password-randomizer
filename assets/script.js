8edb2c880fcd24066c8156ce3470a70e8173e0c6
aedelfrid <almck51@gmail.com>
2023-05-14 22:24:16 -0300

    refactored to remove redundant code and found bug

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var generateBtn = document.querySelector("#generate");
-var password = undefined;
+var password = "";
 var passLen = undefined;
 var criteria = ["lowercase", "uppercase", "numeric", "special"];
 
@@ -18,21 +18,21 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function checkCriteria() {
-  alert("Welcome to the password generator wizard!");
+  alert("Welcome to the password randomizer wizard!");
 
   passLen = prompt("How long would you like the password to be?\nBe aware it must be at least 8 and at most 128 characters.")
 
   if (!confirm("Would you like the password to include special characters?\nYou must select at least one password criteria.")) {
-      criteria.pop()
+    delete criteria["special"]
   };
   if (!confirm("Would you like the password to include numeric characters?\nYou must select at least one password criteria.")) {
-      criteria.pop()
+    delete criteria["numeric"]
   };
   if (!confirm("Would you like the password to include uppercase characters?\nYou must select at least one password criteria.")) {
-      criteria.pop()
+    delete criteria["uppercase"]
   };
   if (!confirm("Would you like the password to include lowercase characters?\nYou must select at least one password criteria.")) {
-      criteria.pop()
+    delete criteria["lowercase"]
   };
 
   validateLen();
@@ -55,35 +55,21 @@ function validate() {
 }
 
 function generate() {
-  let determiner = null;
-  determiner = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
-      if (determiner == 0) {
-          var lower = Math.floor(Math.random() * (25 - 0 + 1)) + 0
-          password = lowercaseCharacters[lower];
-      } else if (determiner == 1) {
-          var upper = Math.floor(Math.random() * (25 - 0 + 1)) + 0;
-          password = uppercaseCharacters[upper];
-      } else if (determiner == 2) {
-          password = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
-      } else if (determiner == 3) {
-          var special = Math.floor(Math.random() * (16 - 0 + 1)) + 0;
-          password = specialCharacters[special];
-      }
-    
+  let determiner = undefined;
   for (let i = 0; i < passLen; i++) {
       determiner = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
-      if (determiner == 0) {
+      if (determiner == 0 && criteria == "lowercase") {
           var lower = Math.floor(Math.random() * (25 - 0 + 1)) + 0;
           password += lowercaseCharacters[lower];
           continue
-      } else if (determiner == 1) {
+      } else if (determiner == 1 && criteria == "uppercase") {
           var upper = Math.floor(Math.random() * (25 - 0 + 1)) + 0;
           password += uppercaseCharacters[upper];
           continue
-      } else if (determiner == 2) {
+      } else if (determiner == 2 && criteria == "numeric") {
           password += Math.floor(Math.random() * (9 - 0 + 1)) + 0;
           continue
-      } else if (determiner == 3) {
+      } else if (determiner == 3 && criteria == "special") {
           var special = Math.floor(Math.random() * (16 - 0 + 1)) + 0;
           password += specialCharacters[special];
           continue
